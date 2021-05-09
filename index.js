11000df184cff2aa3a7644dcebf9c59ba0cd8784
Craig <craigkrikorian@gmail.com>
2021-05-09 19:33:41 -0400

    commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let canvas = document.getElementById('gamecontainer');
 let ctx = canvas.getContext('2d');
-
-function mainloop() {
+let img = new Image();   // Create new img element
+img.src = 'assets/test2.png'; 
+let img2 = new Image();
+img2.src = "assets/test.png";
+function mainLoop() {
+   
     console.log('looping');
+    ctx.drawImage(img , 50, 50);
+    ctx.drawImage(img2 , 100 , 100);
+    console.log("Image 1 width: " + img.width);
+    console.log("Image 2 width: " + img2.width);
 }
-setInterval(mainLoop(), 1000);
+
+setInterval(mainLoop, 500);
